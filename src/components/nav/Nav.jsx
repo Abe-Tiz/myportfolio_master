f57a41215be85016ba4d056ed52ede90ec8dfb1d
Abe-Tiz <106752541+Abe-Tiz@users.noreply.github.com>
2024-05-11 02:11:04 +0300

    added the click on nav links  and refactor code

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,79 +1,79 @@
+
 import React, { useEffect, useState } from "react";
-import './nav.css'
+import "./nav.css";
 import logo from "../../assets/logo.png";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Nav = () => {
-
-  const [activeNav, setActiveNav] = useState('#');
+  const [activeNav, setActiveNav] = useState("#");
   const [showNav, setShowNav] = useState(false);
-    const [isSticky, setSticky] = useState(false);
+  const [isSticky, setSticky] = useState(false);
 
-  // handle scroll functrions
   useEffect(() => {
     const handleScroll = () => {
       const offSet = window.scrollY;
-      if (offSet > 0) {
-        setSticky(true);
-      } else {
-        setSticky(false);
-      }
+      setSticky(offSet > 0);
     };
+
     window.addEventListener("scroll", handleScroll);
+
     return () => {
-      window.addEventListener("scroll", handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-  
+
+  const handleNavClick = (navItem) => {
+    setActiveNav(navItem);
+    setShowNav(!showNav);
+  };
+
   return (
-    <nav className={`container ${isSticky ? " sticky" : ""}`}>
+    <nav className={`container ${isSticky ? "sticky" : ""}`}>
       <a href="#home" className="logo">
-        <img src={logo} alt="logo " />
+        <img src={logo} alt="logo" />
       </a>
       <button className="toggler" onClick={() => setShowNav(!showNav)}>
         {showNav ? <AiOutlineClose /> : <AiOutlineMenu />}
       </button>
       <div className={`nav-items ${showNav ? "show" : ""}`}>
-        {" "}
         <a
           href="#home"
-          onClick={() => setActiveNav("#")}
+          onClick={() => handleNavClick("#")}
           className={activeNav === "#" ? "active" : ""}
         >
           Home
         </a>
         <a
           href="#about"
-          onClick={() => setActiveNav("#about")}
+          onClick={() => handleNavClick("#about")}
           className={activeNav === "#about" ? "active" : ""}
         >
           About
         </a>
         <a
           href="#experience"
-          onClick={() => setActiveNav("#experience")}
+          onClick={() => handleNavClick("#experience")}
           className={activeNav === "#experience" ? "active" : ""}
         >
           Skill
         </a>
         <a
           href="#myprojects"
-          onClick={() => setActiveNav("#myprojects")}
+          onClick={() => handleNavClick("#myprojects")}
           className={activeNav === "#myprojects" ? "active" : ""}
         >
           Project
         </a>
         <a
           href="#contacts"
-          onClick={() => setActiveNav("#contacts")}
+          onClick={() => handleNavClick("#contacts")}
           className={activeNav === "#contacts" ? "active" : "link"}
         >
           Contact
         </a>
       </div>
-      <div></div>
     </nav>
   );
-}
+};
 
-export default Nav
+export default Nav;
